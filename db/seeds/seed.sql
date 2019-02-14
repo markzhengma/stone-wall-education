1de1cf9564526fb041b2efec1227b3938dedadfe
Mark Ma <mazheng1022@hotmail.com>
2019-02-14 13:39:49 -0500

    backend file structures and migration/seed files

---
+++ db/seeds/seed.sql
@@ -0,0 +1,23 @@
+\c stonewall_db;
+
+INSERT INTO resources
+    (title, link, category)
+VALUES
+    ('An LGBTQ History Educators’ Guide', 'https://www.academia.edu/36806202/An_LGBTQ_History_Educators_Guide', 0),
+    ('Teaching Tolerance’s New LGBTQ Best Practices Guide', 'https://www.tolerance.org/magazine/fall-2018/tts-new-lgbtq-best-practices-guide', 0),
+    ('Teaching Tolerance’s Learning Plan: Gender and Sexuality', 'https://www.tolerance.org/learning-plan/gender-and-sexuality-the-purpose-is-to-get-students-thinking-about-social-justice', 0),
+    ('Teaching Tolerance’s Learning Plan: Gender Identity and Sexuality', 'https://www.tolerance.org/learning-plan/gender-identity-and-sexuality', 0),
+    ('Educator Guides for GLSEN Programs', 'https://www.glsen.org/educate/resources/guides', 0),
+    ('LGBT-Inclusive Curriculum', 'https://www.glsen.org/educate/resources/curriculum', 0),
+    ('Critical Media Literacy’s LGBTQ education resources', 'http://criticalmediaproject.org/category/lgbtq/', 0),
+    ('California’s FAIR Ed Act includes lesson plans', 'http://www.faireducationact.com/about-fair/', 0),
+    ('The Gender Unicorn', 'http://www.transstudent.org/gender/', 1),
+    ('Genderbread Person', 'https://www.genderbread.org/', 1),
+    ('Gender Inclusive Language', 'https://www.teenvogue.com/story/how-to-use-gender-neutral-words', 1),
+    ('Gender Spectrum Story Time', 'https://www.youtube.com/watch?v=3rx0q0M61uE&feature=youtu.be', 1),
+    ('Human Rights Campaign: Guide to Supporting Transgender Students in K-12 Schools', 'https://assets2.hrc.org/files/assets/resources/Schools-In-Transition.pdf?_ga=2.81582547.559796857.1539701294-715597900.1539701294', 1),
+    ('Teaching Tolerance’s Learning Plan: Gender Stereotypes and Gender Identity', 'https://www.tolerance.org/learning-plan/gender-stereotypes-and-gender-identity', 1),
+    ('History Channel: How the Stonewall Riots Sparked a Movement', 'https://www.youtube.com/watch?v=Q9wdMJmuBlA', 2),
+    ('Laverne Cox, Candis Cayne & More on Marsha P. Johnson and Sylvia Rivera', 'https://www.youtube.com/watch?v=C-pMeR2lnlo', 2),
+    ('PBS American Experience: Stonewall Uprising', 'https://www.pbs.org/video/american-experience-stonewall-uprising/', 2),
+    ('PBS American Experience: Legacy of the Stonewall Riots', 'https://www.pbs.org/video/american-experience-legacy-stonewall-riots/', 2);
