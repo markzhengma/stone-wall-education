a1d03b2ba76fe80211c172805d7975aa5c1c3258
Mark Ma <mazheng1022@hotmail.com>
2019-02-16 21:00:21 -0500

    add pagination to resource component

--- client/src/Components/ResourcePage.jsx
+++ client/src/Components/ResourcePage.jsx
@@ -7,6 +7,10 @@ class ResourcePage extends Component {
         super();
         this.state = {
             resourceData: null,
+            pageCount: 1,
+            currentPageContent: null,
+            currentPage: 1,
+            pageArray: [],
             category: null,
         }
     }
@@ -16,9 +20,27 @@ class ResourcePage extends Component {
         .then(res => {
             if(res.data && res.data.length >= 1){
                 this.setState({
+                    pageCount: Math.ceil(res.data.length/10),
+                    currentPage: 1,
                     resourceData: res.data,
                     category: "All Resources",
                 });
+                if(res.data.length <= 10){
+                    this.setState({
+                        currentPageContent: res.data,
+                    })
+                }else{
+                    this.setState({
+                        currentPageContent: res.data.slice(0, 10),
+                    })
+                };
+                var pageArray = [];
+                for (var i = 1; i <= Math.ceil(res.data.length/10); i ++){
+                    pageArray.push(i);
+                    this.setState({
+                        pageArray: pageArray,
+                    })
+                }
             }else{
                 console.log("not able to retrieve data");
             }
@@ -33,8 +55,26 @@ class ResourcePage extends Component {
         .then(res => {
             if(res.data && res.data.length >= 1){
                 this.setState({
+                    pageCount: Math.ceil(res.data.length/10),
+                    currentPage: 1,
                     resourceData: res.data,
                 });
+                if(res.data.length <= 10){
+                    this.setState({
+                        currentPageContent: res.data,
+                    })
+                }else{
+                    this.setState({
+                        currentPageContent: res.data.slice(0, 10),
+                    })
+                };
+                var pageArray = [];
+                for (var i = 1; i <= Math.ceil(res.data.length/10); i ++){
+                    pageArray.push(i);
+                    this.setState({
+                        pageArray: pageArray,
+                    })
+                }
                 switch (category){
                     case 0:
                         this.setState({
@@ -43,12 +83,22 @@ class ResourcePage extends Component {
                         break;
                     case 1:
                         this.setState({
-                            category: "Gender Specific",
+                            category: "Gender-Specific Resources",
                         });
                         break;
                     case 2:
                         this.setState({
-                            category: "Stonewall Specific",
+                            category: "Stonewall-Specific Resources",
+                        });
+                        break;
+                    case 3:
+                        this.setState({
+                            category: "Archives",
+                        });
+                        break;
+                    case 4:
+                        this.setState({
+                            category: "Zines",
                         });
                         break;
                     default:
@@ -65,6 +115,23 @@ class ResourcePage extends Component {
         })
     }
 
+    handlePageChange = (pageNum) => {
+        if(pageNum >= 1 && pageNum <= this.state.pageCount){
+            this.setState({
+                currentPage: pageNum
+            })
+            if((this.state.resourceData.length - 1) - (pageNum - 1) * 10 >= 9){
+                this.setState({
+                    currentPageContent: this.state.resourceData.slice(((pageNum - 1) * 10), ((pageNum - 1) * 10 + 10))
+                })
+            }else{
+                this.setState({
+                    currentPageContent: this.state.resourceData.slice(((pageNum - 1) * 10), (this.state.resourceData.length))
+                })
+            }
+        }
+    }
+
     render(){
         return (
             <div className = "resource-page">
@@ -73,15 +140,45 @@ class ResourcePage extends Component {
                     <button onClick = {this.getAllResources}>All Resources</button>
                     <button onClick = {() => this.getResourceByCat(1)}>Gender-Specific</button>
                     <button onClick = {() => this.getResourceByCat(2)}>Stonewall-Specific</button>
+                    <button onClick = {() => this.getResourceByCat(3)}>Archives</button>
+                    <button onClick = {() => this.getResourceByCat(4)}>Zines</button>
                     <button onClick = {() => this.getResourceByCat(0)}>Other</button>
                 </div>
+                {this.state.resourceData ? 
+                    <nav aria-label="...">
+                        <ul className = "pagination">
+                            <li 
+                                className = {this.state.currentPage === 1 ? "page-item disabled" : "page-item"}
+                                onClick = {() => this.handlePageChange(this.state.currentPage - 1)}
+                            >
+                                <a className = "page-link" href = "#">Previous</a>
+                            </li>
+                            {this.state.pageArray.map(pageNum => {
+                                return(
+                                    <li class= {pageNum === this.state.currentPage ? "page-item active" : "page-item"}>
+                                        <a className = "page-link" onClick = {() => this.handlePageChange(pageNum)}>
+                                            {pageNum}
+                                            {pageNum === this.state.currentPage ? <span class="sr-only">(current)</span> : ""} 
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                            <li 
+                                className = {this.state.currentPage === this.state.pageCount ? "page-item disabled" : "page-item"}
+                                onClick = {() => this.handlePageChange(this.state.currentPage + 1)}
+                            >
+                                <a className = "page-link" href = "#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                : ""}
                 <div className = "resource-renderer">
                     {this.state.category ? 
                         <h4>{this.state.category}</h4>
                     : ""}
                     <div className = "resource-list">
-                        {this.state.resourceData != null && this.state.resourceData.length >= 1 ? 
-                            this.state.resourceData.map(data => {
+                        {this.state.currentPageContent != null && this.state.currentPageContent.length >= 1 ? 
+                            this.state.currentPageContent.map(data => {
                                 return (
                                     <div className = "resource-single">
                                         <p>Img Link: {data.img}</p>
@@ -93,6 +190,33 @@ class ResourcePage extends Component {
                         : ""}
                     </div>
                 </div>
+                {this.state.resourceData ? 
+                    <nav aria-label="...">
+                        <ul className = "pagination">
+                            <li 
+                                className = {this.state.currentPage === 1 ? "page-item disabled" : "page-item"}
+                                onClick = {() => this.handlePageChange(this.state.currentPage - 1)}
+                            >
+                                <a className = "page-link" href = "#">Previous</a>
+                            </li>
+                            {this.state.pageArray.map(pageNum => {
+                                return(
+                                    <li class="page-item">
+                                        <a class="page-link" onClick = {() => this.handlePageChange(pageNum)}>
+                                            {pageNum}
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                            <li 
+                                className = {this.state.currentPage === this.state.pageCount ? "page-item disabled" : "page-item"}
+                                onClick = {() => this.handlePageChange(this.state.currentPage + 1)}
+                            >
+                                <a className = "page-link" href = "#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                : ""}
             </div>
         )
     }
