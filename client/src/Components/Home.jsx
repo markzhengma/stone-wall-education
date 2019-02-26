ba4aa7638fb392998b96c0898e267d517b97f1be
Mark <mazheng1022@hotmail.com>
2019-02-25 21:42:17 -0500

    solve merge conflict

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -8,17 +8,10 @@ class Home extends Component {
                 <div className = "home_banner">
                 </div>
                 <div className = "row">
-<<<<<<< HEAD
-                    <div className = "col-sm-3"/>
-                    <div className = "col-sm-6 home_title">
-                        <h1>Stonewall Project-1</h1>
-                        <h3>Digital Curriculum</h3>
-=======
                     <div className = "col-sm-2"/>
                     <div className = "col-sm-8 home_title">
                         <h1>Stonewall Education Resources</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
->>>>>>> e321555b2cb850b7f29cfe883111413315fda916
                     </div>
                     <div className = "col-sm-2"/>
                 </div>
@@ -37,13 +30,6 @@ class Home extends Component {
                             <div className = "search_img"/>
                         </div>
                     </div>
-<<<<<<< HEAD
-                </div>
-            </div>
-            <div className = "highlight_section">
-                <h3>Here's the highlight course list</h3>
-
-=======
                     <div className = "highlight_section">
                         <div className = "hightlight_line"/>
                         <div className = "hightlight_course row">
@@ -56,7 +42,6 @@ class Home extends Component {
                         </div>
                       </div>
                   </div>
->>>>>>> e321555b2cb850b7f29cfe883111413315fda916
             </div>
             <div className = "contact_section">
                 <h3>Contact Us</h3>
